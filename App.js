be83035273b907c8123574e7f46ed480a52bae08
kalanbrinkley <kcbrink1054@gmail.com>
2022-05-28 19:13:33 -0400

    Changes to include fonts

--- App.js
+++ App.js
@@ -3,12 +3,21 @@ import { Box, NativeBaseProvider } from 'native-base';
 import { StyleSheet, Text, View } from 'react-native';
 import { BACKGROUNDCOLOR } from './components/constants';
 import TaskitApp from './components/TaskitApp';
-
+import { useFonts } from "expo-font";
 export default function App() {
+  const [loaded] = useFonts({
+    Arial: require("./assets/fonts/ARIAL.TTF"),
+    ArialBlack: require("./assets/fonts/ARIALBLACK.ttf"),
+  })
+
+  if (!loaded) {
+    return null;
+  }
   return (
     <NativeBaseProvider>
       <Box style={styles.container}>
-        <TaskitApp/>
+        {/* <TaskitApp/> */}
+        <Text>Test</Text>
         <StatusBar style="auto" />
         
       </Box>
--- components/Text.js
+++ components/Text.js
@@ -3,7 +3,7 @@ import React from 'react'
 import { WHITE } from './constants'
 
 export default function CustomText(props) {
-  return ( <Text style={[props.style, {color:WHITE, fontFamily:'arial'}]}>{props.children}</Text>)
+  return ( <Text style={[props.style, {color:WHITE, fontFamily:'Arial'}]}>{props.children}</Text>)
 }
 
 const styles = StyleSheet.create({})
